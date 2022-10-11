29fc68cbd09f6f9b9f6a1cbe143221c949b433f6
Luc-Anne <lucanne.t@gmx.fr>
2022-10-11 14:57:57 +0200

    ajout entité utilisateur avec management

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,6 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
 ];
