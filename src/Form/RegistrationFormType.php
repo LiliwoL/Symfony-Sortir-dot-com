369af45482438695f281f29dd45691887cedccdc
Luc-Anne <lucanne.t@gmx.fr>
2022-10-11 14:59:17 +0200

    ajout entité utilisateur avec management

---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Utilisateur;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'Cocher pour accepter les conditions d\'utilisation.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Merci d\'entrer votre mot de passe',
+                    ]),
+                    new Length([
+                        // TODO Changer min à 16 avant la production
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} lettres et chiffres',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'label' => 'mot de passe'
+            ])
+            ->add('courriel', EmailType::class)
+            ->add('nom')
+            ->add('prenom')
+            ->add('isActif')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Utilisateur::class,
+        ]);
+    }
+}
