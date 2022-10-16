50be3989870326f1b1bbad63bcaed3f67d7e32db
Luc Anne <lucanne.t@gmx.fr>
2022-10-16 18:57:12 +0200

    Ajout du nombre de personnes actives sur le site dans la section Connexion de la page d'accueil

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UtilisateurRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class AccueilController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(): Response
+    public function index(UtilisateurRepository $utilisateurRepository): Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_utlisateur');
         }
 
+        $nbActif = $utilisateurRepository->count(['isActif' => true]);
+
         return $this->render('accueil/index.html.twig', [
             'last_username' => "", // see login
-            'error' => "" // see login
+            'error' => "", // see login
+            'nbActif' => $nbActif
         ]);
     }
 }
