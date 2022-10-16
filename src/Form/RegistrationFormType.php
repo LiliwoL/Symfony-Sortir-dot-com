38a73de57693f044ee4e493f5065a40b4132978b
Luc Anne <lucanne.t@gmx.fr>
2022-10-16 19:09:56 +0200

    Correction du texte du formulaire d'inscription administrateur

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,24 +4,22 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\IsTrue;
-use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
-            ->add('courriel', EmailType::class)
             ->add('nom')
             ->add('prenom')
+            ->add('username', TextType::class, [
+                'label' => 'Pseudo'
+            ])
+            ->add('courriel', EmailType::class)
         ;
     }
 
